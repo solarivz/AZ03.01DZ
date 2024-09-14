a0c1146f2734daa6f966d524e4fce8e87fedbb61
Ivan Zyulin <solar.ivz@gmail.com>
2024-09-14 18:44:00 +1200

    gen 2 random mass

---
+++ main1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Генерация двух массивов случайных чисел
+x_data = np.random.rand(100)
+y_data = np.random.rand(100)
+
+# Построение диаграммы рассеяния
+plt.scatter(x_data, y_data, c='blue', alpha=0.5)
+plt.title('Диаграмма рассеяния для случайных данных')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
+
