a6558a0c9da3a7bbe0e7145e28a6e58c117446a2
Ivan Zyulin <solar.ivz@gmail.com>
2024-09-14 18:19:49 +1200

    add param

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Параметры нормального распределения
+mean = 0        # Среднее значение
+std_dev = 1     # Стандартное отклонение
+num_samples = 1000  # Количество образцов
+
+
+
