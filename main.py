2ac85bfda75c88d54b0334a92b1595876b7aea2d
Ivan Zyulin <solar.ivz@gmail.com>
2024-09-14 18:22:26 +1200

    add random func  and  plt

--- main.py
+++ main.py
@@ -7,4 +7,13 @@ std_dev = 1     # Стандартное отклонение
 num_samples = 1000  # Количество образцов
 
 
+# Генерация случайных чисел, распределенных по нормальному распределению
+data = np.random.normal(mean, std_dev, num_samples)
+
+# Создание гистограммы
+plt.hist(data, bins=30, edgecolor='black')
+plt.title('Гистограмма случайных данных (Нормальное распределение)')
+plt.xlabel('Значения')
+plt.ylabel('Частота')
+plt.show()
 
